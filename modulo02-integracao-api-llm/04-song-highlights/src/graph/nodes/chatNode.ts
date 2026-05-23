7c5f41245f6ae4b7867b767a323f51ae4ffddffe
Henrique Hupp <henriquehupp@MBA.local>
2026-05-22 21:11:10 -0300

    finish: 04 - Song Recomender

---
+++ modulo02-integracao-api-llm/04-song-highlights/src/graph/nodes/chatNode.ts
@@ -0,0 +1,51 @@
+import type { Runtime } from '@langchain/langgraph';
+import { OpenRouterService } from '../../services/openrouterService.ts';
+import type { GraphState } from '../graph.ts';
+import { ChatResponseSchema, getSystemPrompt, getUserPromptTemplate } from '../../prompts/v1/chatResponse.ts';
+import { AIMessage, HumanMessage } from 'langchain';
+import { PreferencesService } from '../../services/preferencesService.ts';
+import { config } from '../../config.ts';
+
+export function createChatNode(llmClient: OpenRouterService, preferencesService: PreferencesService) {
+  return async (state: GraphState, runtime?: Runtime): Promise<Partial<GraphState>> => {
+
+    const userId = String(runtime?.context?.userId || state.userId || 'unknown-user');
+    const userContext = state.userContext ?? await preferencesService.getBasicInfo(userId)
+
+    const systemPrompt = getSystemPrompt(userContext)
+    const conversationHistory = state.messages
+      .map(msg => `${HumanMessage.isInstance(msg) ? 'User' : 'AI'}: ${msg.content}`)
+      .join('\n');
+
+    const userMessage = state.messages.at(-1)?.text;
+    const userPrompt = getUserPromptTemplate(
+      userMessage as string,
+      conversationHistory
+    )
+
+    const result = await llmClient.generateStructured(
+      systemPrompt,
+      userPrompt,
+      ChatResponseSchema
+    )
+
+    if (!result.success || !result.data) {
+      console.error('Erro ao gerar resposta estruturada:', result.error);
+      return {
+        messages: [
+          new AIMessage('Desculpe, ocorreu um erro. Por favor, tente novamente mais tarde.')
+        ]
+      };
+    }
+
+    const response = result.data
+    const totalMessages = state.messages.length
+    const needsSummarization = totalMessages >= config.maxMessagesToSummary
+
+    return {
+      messages: [new AIMessage(response.message)],
+      extractedPreferences: response.shouldSavePreferences ? response.preferences : undefined,
+      needsSummarization,
+    };
+  };
+}
